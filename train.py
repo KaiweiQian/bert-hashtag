34d8cbb1dda8b8a98cbb80b9bd43e675974838d2
Kaiwei Qian <kevinqian1993@hotmail.com>
2019-11-25 09:38:28 -0500

    add code that trains the model

--- train.py
+++ train.py
@@ -8,17 +8,33 @@ from models import BertHashtag
 
 
 if __name__ == '__main__':
-    data = TweetDataset(file_path='./data/train.txt',
-                        meta_path='./data/meta.txt',
-                        max_len=256)
+    device = torch.device('cuda:0')
+
+    train_data = TweetDataset(file_path='./data/train.txt',
+                              meta_path='./data/meta.txt',
+                              max_len=256)
+
+    dev_data = TweetDataset(file_path='./data/dev.txt',
+                            meta_path='./data/meta.txt',
+                            max_len=256)
+
     tweet_model = BertHashtag.from_pretrained('bert-base-uncased')
+    loss_func = CrossEntropyLoss(reduction='mean')
+
     optimizer = Adam(tweet_model.parameters(), lr=5e-4)
 
-    train_dataloader = DataLoader(data, batch_size=32, shuffle=True)
+    train_dataloader = DataLoader(train_data, batch_size=32, shuffle=True)
 
-    for it, batch in enumerate(train_dataloader):
-        token_ids, token_type_ids, attention_mask, label = batch
-        logits = tweet_model(input_ids=token_ids,
-                             token_type_ids=token_type_ids,
-                             attention_mask=attention_mask
-                             )
+    for it, (token_ids, token_type_ids, attn_mask, label) in enumerate(train_dataloader):
+        optimizer.zero_grad()
+
+        token_ids, token_type_ids, attn_mask = token_ids.to(device), token_type_ids.to(device), attn_mask.to(device)
+        label = label.to(device)
+
+        score = tweet_model(input_ids=token_ids,
+                            token_type_ids=token_type_ids,
+                            attention_mask=attn_mask
+                            )
+
+        loss = loss_func(score, label)
+        loss.backward()
