09b697aa8fcfe867408073956fbe2fc504940cb1
Kaiwei Qian <kevinqian1993@hotmail.com>
2019-11-26 09:29:04 -0500

    Updated the dataset. Changed the batch size and max_len.

--- train.py
+++ train.py
@@ -8,15 +8,22 @@ from models import BertHashtag
 
 
 if __name__ == '__main__':
+    warm_start = False
+    PATH = './checkpoints/'
+
     device = torch.device('cuda:0')
-    n_epoch = 20
+    n_epoch = 50
 
     train_data = TweetDataset(file_path='./data/train.txt',
                               meta_path='./data/meta.txt',
-                              max_len=256)
+                              max_len=64)
 
     tweet_model = BertHashtag()
     tweet_model = tweet_model.to(device)
+
+    if warm_start:
+        tweet_model.load_state_dict(torch.load(PATH)['model_state_dict'])
+
     tweet_model.train()
 
     loss_func = CrossEntropyLoss(reduction='mean')
@@ -25,7 +32,7 @@ if __name__ == '__main__':
 
     for epoch in range(n_epoch):
         cum_loss = 0
-        train_dataloader = DataLoader(train_data, batch_size=8, shuffle=True)
+        train_dataloader = DataLoader(train_data, batch_size=32, shuffle=True)
         print('Epoch {} starts!'.format(epoch+1))
 
         for it, (token_ids, token_type_ids, attn_mask, label) in enumerate(train_dataloader):
@@ -51,9 +58,9 @@ if __name__ == '__main__':
 
         save_name = './checkpoints/tweet_model_gpu_checkpoints_epoch_{}.tar'.format(epoch+1)
 
-        torch.save({'epoch': epoch+1,
-                    'model_state_dict': tweet_model.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
-                    'loss': loss},
-                   save_name
-                   )
+        if (epoch + 1) % 5 == 0:
+            torch.save({'epoch': epoch + 1,
+                        'model_state_dict': tweet_model.state_dict(),
+                        'loss': loss},
+                       save_name
+                       )
