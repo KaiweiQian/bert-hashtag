a60de6d348ecf4c00f1ae172341cb00149d5ae66
Kaiwei Qian <kevinqian1993@hotmail.com>
2019-12-06 11:36:37 -0500

    Added a file to evaluate the model on development set.

--- evaluate.py
+++ evaluate.py
@@ -8,11 +8,15 @@ from torch.utils.data import DataLoader
 if __name__ == '__main__':
     model_path = './checkpoints/tweet_3_class_gpu_checkpoints_lr_1e-07_epoch_20.tar'
     max_len = 32
+    device = torch.device('cuda:0')
 
     bert = BertHashtag(num_class=3)
+    bert = bert.to(device)
+
     bert.load_state_dict(torch.load(model_path)['model_state_dict'])
 
     dev = TweetDataset('./data/dev.txt', './data/meta.txt', max_len)
+    dev = dev.to(device)
 
     bert.eval()
     dev_dataloader = DataLoader(dev, batch_size=512, shuffle=False)
@@ -28,9 +32,9 @@ if __name__ == '__main__':
                    )
         v, k = torch.max(out, -1)
 
-        y_true += label.numpy().tolist()
-        y_pred += k.numpy().tolist()
-        y_pred_prob += v.numpy().tolist()
+        y_true += label.numpy().cpu().tolist()
+        y_pred += k.numpy().cpu().tolist()
+        y_pred_prob += v.detach().numpy().cpu().tolist()
 
     f1 = f1_score(y_true=y_true, y_pred=y_pred, average='macro')
     acc = accuracy_score(y_true=y_true, y_pred=y_pred)
